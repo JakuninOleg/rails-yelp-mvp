a58f5fe8863d078e0540a5ed9cc5896ea4930aad
Oleg Jakunin <oleg.kemz@gmail.com>
2017-06-07 11:50:02 +0200

    models are working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates_presence_of :name
+  validates_presence_of :address
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false}
 end
